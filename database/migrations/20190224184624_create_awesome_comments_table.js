2efab5a5c50424f18a7543f1770011e745227a93
buuug7 <youpp@126.com>
2019-02-25 00:22:35 +0800

    add awesome_comment_user_stars migration table

--- database/migrations/20190224184624_create_awesome_comments_table.js
+++ database/migrations/20190224184624_create_awesome_comments_table.js
@@ -1,15 +1,38 @@
 exports.up = function (knex, Promise) {
-  return knex.schema.createTable('awesome_comments', function (table) {
-    table.increments('id').primary()
-    table.integer('user_id').notNullable()
-    table.text('content').notNullable()
-    table.string('reference')
-    table.boolean('active').defaultTo(false)
-    table.dateTime('deleted_at')
-    table.timestamps()
-  })
+
+  return createAwesomeComments().then(createAwesomeCommentUserStars)
+
+  function createAwesomeComments () {
+    return knex.schema.createTable('awesome_comments', function (table) {
+      table.increments('id').primary()
+      table.integer('user_id').notNullable()
+      table.text('content').notNullable()
+      table.string('reference')
+      table.boolean('active').defaultTo(false)
+      table.dateTime('deleted_at')
+      table.timestamps()
+    })
+  }
+
+  function createAwesomeCommentUserStars () {
+    return knex.schema.createTable('awesome_comment_user_stars',
+      function (table) {
+        table.integer('awesome_comment_id')
+        table.integer('user_id')
+        table.primary(['awesome_comment_id', 'user_id'])
+        table.timestamps()
+      })
+  }
 }
 
 exports.down = function (knex, Promise) {
-  return knex.schema.dropTableIfExists('awesome_comments')
+  return dropAwesomeComments().then(dropAwesomeCommentUserStars)
+
+  function dropAwesomeComments () {
+    return knex.schema.dropTableIfExists('awesome_comments')
+  }
+
+  function dropAwesomeCommentUserStars () {
+    return knex.schema.dropTableIfExists('awesome_comment_user_stars')
+  }
 }
