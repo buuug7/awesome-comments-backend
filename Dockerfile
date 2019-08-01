f15472d9b018ad15f43623a0243872105bc65933
buuug7 <puguohong@gmail.com>
2019-08-01 23:47:39 +0800

    add docker support

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:10
+
+WORKDIR /app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . ./
+
+EXPOSE 80
+
+CMD ["npm", "prod"]
+
--- ormconfig.js
+++ ormconfig.js
@@ -1,20 +1,44 @@
-module.exports = {
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '123456789',
-  database: 'soup_hub',
-  synchronize: true,
-  logging: true,
-  entities: ['src/entity/**/*.ts'],
-  migrations: ['src/migration/**/*.ts'],
-  subscribers: ['src/subscriber/**/*.ts'],
-  cli: {
-    entitiesDir: 'src/entity',
-    migrationsDir: 'src/migration',
-    subscribersDir: 'src/subscriber'
+module.exports = [
+  {
+    name: 'default',
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'test',
+    password: 'test',
+    database: 'soup_hub',
+    synchronize: true,
+    logging: true,
+    entities: ['src/entity/**/*.ts'],
+    migrations: ['src/migration/**/*.ts'],
+    subscribers: ['src/subscriber/**/*.ts'],
+    cli: {
+      entitiesDir: 'src/entity',
+      migrationsDir: 'src/migration',
+      subscribersDir: 'src/subscriber'
+    },
+    seeds: ['src/seeds/**/*.seed.ts'],
+    factories: ['src/factories/**/*.factory.ts']
   },
-  seeds: ['src/seeds/**/*.seed.ts'],
-  factories: ['src/factories/**/*.factory.ts']
-};
+  {
+    name: 'prod',
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'test',
+    password: 'test',
+    database: 'soup_hub',
+    synchronize: true,
+    logging: false,
+    entities: ['build/entity/**/*.js'],
+    migrations: ['build/migration/**/*.js'],
+    subscribers: ['build/subscriber/**/*.js'],
+    cli: {
+      entitiesDir: 'build/entity',
+      migrationsDir: 'build/migration',
+      subscribersDir: 'build/subscriber'
+    },
+    seeds: ['build/seeds/**/*.seed.js'],
+    factories: ['build/factories/**/*.factory.js']
+  }
+];
